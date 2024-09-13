24c532bffad9944a64a6632137fe53144e26d7ea
ylnhari <ylnharimailme@gmail.com>
2024-09-13 16:25:09 +0530

    Update build_and_push_to_gcp.sh

--- ml-model-deployment-prod-gcp/build_and_push_to_gcp.sh
+++ ml-model-deployment-prod-gcp/build_and_push_to_gcp.sh
@@ -4,9 +4,11 @@ gcloud auth login
 # Set the Project
 export PROJECT_ID=$(gcloud config get-value project)
 
-# Build the Docker Image , Replace ,region, your-image-name and $TAG_NAME with appropriate values.
-docker build -t <region>.pkg.dev/$PROJECT_ID/custom-image-registry/your-image-name:$TAG_NAME .
+#Replace region, your-image-name and $TAG_NAME with appropriate values.
+export CONTAINER_LINK = <region>.pkg.dev/$PROJECT_ID/custom-image-registry/your-image-name:$TAG_NAME
 
-#Push the Image to GCR:
-gcloud auth configure-docker
-docker push gcr.io/$PROJECT_ID/your-image-name:$TAG_NAME
+#Build the Docker Image , 
+docker build -t  $CONTAINER_LINK Dockerfile.dockerfile .
+
+#Push the Image to GCR
+docker push $CONTAINER_LINK
